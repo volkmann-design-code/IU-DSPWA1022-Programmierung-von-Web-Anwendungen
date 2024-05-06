f7eefdce951c9e4428fd81fb3d6d9cb1642d5dcb
Enzo Volkmann <enzo@volkmann.dev>
2024-05-06 07:44:58 +0200

    remove unused import

--- packages/Challenges/app/src/test/java/challenges/Bank/BankTest.java
+++ packages/Challenges/app/src/test/java/challenges/Bank/BankTest.java
@@ -5,7 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
