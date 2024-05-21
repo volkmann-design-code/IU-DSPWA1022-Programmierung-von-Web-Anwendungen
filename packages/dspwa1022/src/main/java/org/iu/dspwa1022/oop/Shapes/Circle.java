b42b2ec119e30a52ca6c058caf59def4d4484601
Hubi2592 <133772535+Hubi2592@users.noreply.github.com>
2024-05-21 13:13:11 +0200

    Fertig

--- packages/dspwa1022/src/main/java/org/iu/dspwa1022/oop/Shapes/Circle.java
+++ packages/dspwa1022/src/main/java/org/iu/dspwa1022/oop/Shapes/Circle.java
@@ -10,7 +10,7 @@ public class Circle implements Shape {
 
     @Override
     public double getArea() {
-        return Math.PI * radius * radius;
+        return Math.PI *  (radius*2);
     }
 
     @Override
